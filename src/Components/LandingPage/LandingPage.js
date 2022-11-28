557af58cdcced4bcdb571ebb63a4fe741b87a5d0
Abhishek Patra <110832121+abhishek-19091997@users.noreply.github.com>
2022-11-28 20:05:32 +0530

    landing page-1st commit & Branch Creation

---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,262 @@
+import React from "react";
+import "./landingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="mainPage">
+      <h2>PREMIUM BIOTIQUES</h2>
+      <div className="cardDiv">
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28381.webp"
+            width="399px"
+            alt=""
+          />
+          <p>And..It's A Brilinat Shot...!| Up To 12+Y</p>
+          <p>Curated Collection For Football Fans</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28345.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Ho-Ho-Ho Christmas Collection | Up to 12+Y</p>
+          <p>Party wear, Christmas Costumes, Gifts & more</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28386.webp"
+            width="399px"
+            alt=""
+          />
+          <p>The Black Friday Sale | Up to 12+Y</p>
+          <p>Special Collection For The Season</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28378.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Nike + Jordan + Converse + Levi's</p>
+          <p>Top Brands Top Offers | 4-12+Y</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28398.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Wedding Tales | Up to 12+Y</p>
+          <p>Trendy Styles to flaunt this season</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28392.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Sweater Weather | 2 - 12+Y</p>
+          <p>Hand Picked Winter Sweaters for Lil Ones</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28388.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Eco-friendly Mom Store</p>
+          <p>Fabulous Maternity Wear for Moms Out There</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28379.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Bling it on | Up to 24M</p>
+          <p>Curated Collection of Party Dresses, Suits & more</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28400.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Cheerful Picks | Up to 24M</p>
+          <p>Bodysuits, Sets & more</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28387.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Say Tees... |Up to 24M</p>
+          <p>Full Sleeves Tops & T-shirts for Winter</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28380.webp"
+            width="399px"
+            alt=""
+          />
+          <p>All You Need This Winter | Up to 24M</p>
+          <p>Sets & suits, Onesies & Rompers, Frocks & more</p>
+        </div>
+        <div className="card">
+          <img
+            src="//cdn.fcglcdn.com/brainbees/images/boutique/670x670/28401.webp"
+            width="399px"
+            alt=""
+          />
+          <p>Trendsetter | 4 - 12+Y</p>
+          <p>Jeans, Tshirts, tops & more</p>
+        </div>
+      </div>
+      <div className="catagoryCard">
+        <div className="catagory">
+          <p>View All Biotiques</p>
+        </div>
+        <div className="catagory">
+          <p>View All Premium Brands</p>
+        </div>
+      </div>
+      <div>
+        <img
+          width="90%"
+          src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/desktop_wedding22_theme_181122_01.jpg"
+          alt=""
+        />
+        <img
+          className="weddingImage"
+          width="90%"
+          src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/desktop_wedding22_theme_181122_02.jpg"
+          alt=""
+        />
+        <img
+          className="weddingImage"
+          width="90%"
+          src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/desktop_wedding22_theme_181122_03.jpg"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          className="weddingImage"
+          width="90%"
+          src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_01.jpg"
+          alt=""
+        />
+        <img
+          className="weddingImage"
+          width="90%"
+          src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_02.jpg"
+          alt=""
+        />
+        <img
+          className="weddingImage"
+          width="90%"
+          src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_03.jpg"
+          alt=""
+        />
+      </div>
+       {/* Start of Winter Essential */}
+      <div>
+        <img
+          className="weddingImage"
+          width="90%"
+          src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/1920_175_desktop_heavywear_281022_04.jpg"
+          alt=""
+        />
+        <div className="weddingImage">
+          <img
+            width="165px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_05.jpg"
+            alt=""
+          />
+          <img
+            width="165px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_06.jpg"
+            alt=""
+          />
+          <img
+            width="165px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_07.jpg"
+            alt=""
+          />
+          <img
+            width="165px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_08.jpg"
+            alt=""
+          />
+          <img
+            width="165px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_09.jpg"
+            alt=""
+          />
+          <img
+            width="165px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_10.jpg"
+            alt=""
+          />
+          <img
+            width="165px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_11.jpg"
+            alt=""
+          />
+          <img
+            width="165px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_12.jpg"
+            alt=""
+          />
+        </div>
+        <img
+          className="weddingImage"
+          width="90%"
+          src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/1920_179_desktop_heavywear_281022_13.jpg"
+          alt=""
+        />
+        {/* End of Winter Essential */}
+        <div className="weddingImage">
+          <img
+            
+            width="265px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_14.jpg"
+            alt=""
+          />
+          <img
+            width="265px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_15.jpg"
+            alt=""
+          />
+          <img
+            width="265px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_16.jpg"
+            alt=""
+          />
+          <img
+            width="265px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_17.jpg"
+            alt=""
+          />
+          <img
+            width="265px"
+            src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/fashion_desktop_heavywinter_281022_18.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      {/* Beloved Brands */}
+      <img
+        className="weddingImage"
+        width="90%"
+        src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/1920_164_desktop_nnnnn__heavywear_281022_31.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default LandingPage;
